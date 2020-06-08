b43fe67d682f0e05aa2906055c7a340345cb1cb3
Eourist <50326501+Eourist@users.noreply.github.com>
2020-06-08 18:59:14 -0300

    Clases vacias y Conexion

---
+++ src/grupo3pf/Conexion.java
@@ -0,0 +1,34 @@
+package grupo3pf;
+import java.sql.*;
+
+public class Conexion {
+    private static String url;
+    private static String usuario;
+    private static String clave;
+
+    private static Connection conexion;
+    
+    public Conexion(){
+        this.url = "jdbc:mariadb://localhost:3306/universidad";
+        this.usuario = "root";
+        this.clave = "";
+    }
+    
+    public Conexion(String url, String usuario, String password){
+        this.url = url;
+        this.usuario = usuario;
+        this.clave = password;
+    }
+    
+    public static Connection get(){
+        if(conexion == null){
+            try{
+                conexion = DriverManager.getConnection(url, usuario, clave);
+            } catch (SQLException e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        return conexion;
+    }
+}
+
--- src/grupo3pf/Grupo3PF.java
+++ src/grupo3pf/Grupo3PF.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo3pf;
 
-/**
- *
- * @author Eourist
- */
+import java.time.*;
+
 public class Grupo3PF {
 
-    /**
-     * @param args the command line arguments
-     */
+    static enum DiaSemana {LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO};
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        LocalDate ld = LocalDate.of(2000, Month.MARCH, 15);
+        LocalTime lt = LocalTime.of(20, 20); // horas para los horarios
+        
+        LocalTime lll = LocalTime.now();
+        DiaSemana dt = DiaSemana.JUEVES;
+        DayOfWeek dj = DayOfWeek.of(4);  // 1 = lunes, 7 = domingo
+        System.out.println(dj); // THURSDAY
+        System.out.println(dt); // JUEVES
     }
     
 }
