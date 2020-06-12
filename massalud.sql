db5940b7b397789207370edc9e0245d24b9ccfdf
Diego Spartano <50326501+Eourist@users.noreply.github.com>
2020-06-12 11:48:44 -0300

    SQL

---
+++ massalud.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-06-2020 a las 16:45:42
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `massalud`
+--
+CREATE DATABASE `massalud`;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `afiliados`
+--
+
+CREATE TABLE `massalud`.`afiliados` (
+  `id` int(11) NOT NULL,
+  `dni` int(8) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidades`
+--
+
+CREATE TABLE `massalud`.`especialidades` (
+  `id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
+
+CREATE TABLE `massalud`.`horarios` (
+  `id` int(11) NOT NULL,
+  `idPrestador` int(11) NOT NULL,
+  `dia` tinyint(1) NOT NULL COMMENT '1L-7D',
+  `horaInicio` time NOT NULL,
+  `horaFinal` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordenes`
+--
+
+CREATE TABLE `massalud`.`ordenes` (
+  `id` int(11) NOT NULL,
+  `idAfiliado` int(11) NOT NULL,
+  `idHorario` int(11) NOT NULL,
+  `fechaAtencion` date NOT NULL,
+  `fechaCreacion` date NOT NULL,
+  `activa` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestadores`
+--
+
+CREATE TABLE `massalud`.`prestadores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `idEspecialidad` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `afiliados`
+--
+ALTER TABLE `massalud`.`afiliados`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `especialidades`
+--
+ALTER TABLE `massalud`.`especialidades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `horarios`
+--
+ALTER TABLE `massalud`.`horarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPrestador` (`idPrestador`);
+
+--
+-- Indices de la tabla `ordenes`
+--
+ALTER TABLE `massalud`.`ordenes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idAfiliado` (`idAfiliado`),
+  ADD KEY `idHorario` (`idHorario`);
+
+--
+-- Indices de la tabla `prestadores`
+--
+ALTER TABLE `massalud`.`prestadores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idEspecialidad` (`idEspecialidad`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `afiliados`
+--
+ALTER TABLE `massalud`.`afiliados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `especialidades`
+--
+ALTER TABLE `massalud`.`especialidades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ordenes`
+--
+ALTER TABLE `massalud`.`ordenes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `especialidades`
+--
+ALTER TABLE `massalud`.`especialidades`
+  ADD CONSTRAINT `especialidades_ibfk_1` FOREIGN KEY (`id`) REFERENCES `prestadores` (`idEspecialidad`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `horarios`
+--
+ALTER TABLE `massalud`.`horarios`
+  ADD CONSTRAINT `horarios_ibfk_2` FOREIGN KEY (`idPrestador`) REFERENCES `prestadores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ordenes`
+--
+ALTER TABLE `massalud`.`ordenes`
+  ADD CONSTRAINT `ordenes_ibfk_1` FOREIGN KEY (`idAfiliado`) REFERENCES `afiliados` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ordenes_ibfk_3` FOREIGN KEY (`idHorario`) REFERENCES `horarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
