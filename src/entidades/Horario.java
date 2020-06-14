9d60533de4a8f396f1b9e56b3fd1cc44a482aafc
Nehutrew <50326654+Nehuetrew@users.noreply.github.com>
2020-06-14 20:56:53 -0300

    Entidades y atributos

--- src/entidades/Horario.java
+++ src/entidades/Horario.java
@@ -1,5 +1,76 @@
 package entidades;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Horario {
+    private int id;
+    private Prestador prestador;
+    private DayOfWeek dia;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+
+    public Horario(int id, Prestador prestador, DayOfWeek dia, LocalTime horaInicio, LocalTime horaFin) {
+        this.id = id;
+        this.prestador = prestador;
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+
+    public Horario(Prestador prestador, DayOfWeek dia, LocalTime horaInicio, LocalTime horaFin) {
+        this.prestador = prestador;
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Prestador getPrestador() {
+        return prestador;
+    }
+
+    public void setPrestador(Prestador prestador) {
+        this.prestador = prestador;
+    }
+
+    public DayOfWeek getDia() {
+        return dia;
+    }
+
+    public void setDia(DayOfWeek dia) {
+        this.dia = dia;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+    
+    @Override
+    public String toString(){
+        return horaInicio.toString() + " - " + horaFin.toString();
+    }
+    
+    
     
 }
--- src/entidades/Orden.java
+++ src/entidades/Orden.java
@@ -1,5 +1,95 @@
 package entidades;
 
+import java.time.LocalDate;
+
 public class Orden {
+    private int id;
+    private Afiliado afiliado;
+    private Horario horario;
+    private boolean efectivo;
+    private LocalDate fechaAtencion;
+    private LocalDate fechaCreacion;
+    private boolean activa;
+
+    public Orden(int id, Afiliado afiliado, Horario horario, boolean efectivo, LocalDate fechaAtencion, LocalDate fechaCreacion, boolean activa) {
+        this.id = id;
+        this.afiliado = afiliado;
+        this.horario = horario;
+        this.efectivo = efectivo;
+        this.fechaAtencion = fechaAtencion;
+        this.fechaCreacion = fechaCreacion;
+        this.activa = activa;
+    }
+
+    public Orden(Afiliado afiliado, Horario horario, boolean efectivo, LocalDate fechaAtencion, LocalDate fechaCreacion, boolean activa) {
+        this.afiliado = afiliado;
+        this.horario = horario;
+        this.efectivo = efectivo;
+        this.fechaAtencion = fechaAtencion;
+        this.fechaCreacion = fechaCreacion;
+        this.activa = activa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Afiliado getAfiliado() {
+        return afiliado;
+    }
+
+    public void setAfiliado(Afiliado afiliado) {
+        this.afiliado = afiliado;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
+
+    public boolean isEfectivo() {
+        return efectivo;
+    }
+
+    public void setEfectivo(boolean efectivo) {
+        this.efectivo = efectivo;
+    }
+
+    public LocalDate getFechaAtencion() {
+        return fechaAtencion;
+    }
+
+    public void setFechaAtencion(LocalDate fechaAtencion) {
+        this.fechaAtencion = fechaAtencion;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
     
+    
+    @Override
+    public String toString(){
+        return "[" + fechaAtencion.toString() + "] " + horario.toString();
+    }
+
 }
