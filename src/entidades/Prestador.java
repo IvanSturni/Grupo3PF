04bd4b1ca304424f509067365b9e55024d448c24
Nehutrew <50326654+Nehuetrew@users.noreply.github.com>
2020-06-14 21:25:36 -0300

    Prestador

--- src/entidades/Prestador.java
+++ src/entidades/Prestador.java
@@ -1,5 +1,68 @@
 package entidades;
 
 public class Prestador {
+    private int id;
+    private int dni;
+    private String nombre;
+    private Especialidad especialidad;
+    private boolean avtivo;
+
+    public Prestador(int id, int dni, String nombre, Especialidad especialidad, boolean avtivo) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.avtivo = avtivo;
+    }
+
+    public Prestador(int dni, String nombre, Especialidad especialidad, boolean avtivo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.avtivo = avtivo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isAvtivo() {
+        return avtivo;
+    }
+
+    public void setAvtivo(boolean avtivo) {
+        this.avtivo = avtivo;
+    }
+    
+    
+    
     
 }
