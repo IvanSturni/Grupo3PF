2febfe98b61dc94b4339aa3269a901b245838c37
Nehutrew <50326654+Nehuetrew@users.noreply.github.com>
2020-06-17 15:04:30 -0300

    Modelos prestadorData y  especialidadData

--- src/modelos/EspecialidadData.java
+++ src/modelos/EspecialidadData.java
@@ -1,5 +1,66 @@
 package modelos;
+import entidades.*;
+import grupo3pf.*;
+import java.sql.*;
+import java.util.*;
 
 public class EspecialidadData {
     
+    public Especialidad altaEspecialidad(Especialidad especialidad){
+        try {
+            String sql = "INSERT INTO especialidades SET 'nombre' = '" + especialidad.getNombre() + "';";
+            Statement s = Conexion.get().createStatement();
+            s.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = s.getGeneratedKeys();
+            
+            if (rs.first())
+                especialidad.setId(rs.getInt(1));
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error al insertar especialidad: " + e.getMessage());
+        }
+        return especialidad;
+    }
+    
+    public Especialidad obtenerEspecialidad(int id){
+        Especialidad especialidad = null;
+        try {
+            String sql = "SELECT * FROM especialidad WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            ResultSet rs = s.executeQuery(sql);
+            
+            while(rs.next()){
+                especialidad = new Especialidad(rs.getInt("id"), rs.getString("nombre"));
+            }      
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error al obtener especialidad: " + e.getMessage());
+        }
+        return especialidad;
+    }
+    
+    
+    public void bajaEspecialidad(int id){
+        try {
+            String sql = "DELETE FROM especialidad WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            s.executeQuery(sql);
+            
+            s.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al eliminar especialidad: " + ex.getMessage());
+        }
+    }
+    
+    public void actualizarEspecialidad(int id, Afiliado afiliado){
+        try {
+            String sql = "UPDATE especialidad SET  nombre = '" + afiliado.getNombre()+ "' WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            s.execute(sql);
+            
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error al actualizar especialidad: " + e.getMessage());
+        }
+    }
 }
--- src/modelos/PrestadorData.java
+++ src/modelos/PrestadorData.java
@@ -1,5 +1,85 @@
 package modelos;
-
+import entidades.*;
+import grupo3pf.*;
+import java.sql.*;
+import java.util.*;
 public class PrestadorData {
     
+    public Prestador altaPrestador(Prestador prestador){
+         try {
+            String sql = "INSERT INTO prestador (dni, nombre, especialidad, activo) VALUES ('" + prestador.getDni() + "', '" + prestador.getNombre() + "', " + prestador.getEspecialidad() + "', " + (prestador.isActivo() ? "1" : "0") + ");";
+            Statement s = Conexion.get().createStatement();
+            s.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = s.getGeneratedKeys();
+            
+            if (rs.first())
+                prestador.setId(rs.getInt(1));
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error al insertar prestador: " + e.getMessage());
+        }
+         return prestador;
+    }
+    
+    public Prestador obtenerPrestador(int id){
+        Prestador prestador = null;
+        EspecialidadData em = new EspecialidadData();
+        try {
+            String sql = "SELECT * FROM prestador WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            ResultSet rs = s.executeQuery(sql);
+            
+            while(rs.next()){
+                prestador = new Prestador(rs.getInt("id"), rs.getInt("dni"), rs.getString("nombre"), (Especialidad)em.obtenerEspecialidad(rs.getInt("idespecialidad")), rs.getBoolean("activo"));
+            }      
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error al obtener prestador: " + e.getMessage());
+        }
+        return prestador;
+    }
+    
+    public ArrayList<Prestador> obtenerPrestadores(){
+        ArrayList<Prestador> resultados = new ArrayList<>();
+        EspecialidadData em = new EspecialidadData();
+        
+        try {
+            String sql = "SELECT * FROM prestador";
+            Statement s = Conexion.get().createStatement();
+            ResultSet rs = s.executeQuery(sql);
+            
+            while (rs.next()){
+                resultados.add(new Prestador(rs.getInt("id"), rs.getInt("dni"), rs.getString("nombre"), (Especialidad)em.obtenerEspecialidad(rs.getInt("idespecialidad")), rs.getBoolean("activo")));
+            }
+            s.close();
+        } catch (SQLException e){
+            System.out.println("Error al obtener prestadores:" + e.getMessage());
+        }
+        
+        return resultados;
+    }
+    
+    public void bajaPrestador(int id){
+        try {
+            String sql = "DELETE FROM prestador WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            s.executeQuery(sql);
+            
+            s.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al eliminar prestador: " + ex.getMessage());
+        }
+    }
+    
+    public void actualizarPrestador(int id, Prestador prestador){
+        try {
+            String sql = "UPDATE prestador SET dni = '" + prestador.getDni() + "', nombre = '" + prestador.getNombre() + "', activo = " + prestador.getEspecialidad() + "', " + (prestador.isActivo() ? "1" : "0") + " WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            s.execute(sql);
+            
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error al actualizar prestador: " + e.getMessage());
+        }
+    }
 }
